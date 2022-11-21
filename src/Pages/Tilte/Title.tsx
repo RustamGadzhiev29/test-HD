4617f6d852f56315642fd388d46f33cd92e0e425
Rustam <changethe@mail.ru>
2022-11-21 17:42:20 +0300

    refactoring Title.tsx

--- src/Pages/Tilte/Title.tsx
+++ src/Pages/Tilte/Title.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import style from "./Title.module.scss";
+
 export const Title = () => {
-  return <div className={style.titleContainer}>
-    <div className={style.title}>Исторические даты</div>;
-    <div className={style.titleLine}></div>
+  return (
+    <div className={style.titleContainer}>
+      <div className={style.title}>Исторические даты</div>;
+      <div className={style.titleLine} />
     </div>
+  );
 };
